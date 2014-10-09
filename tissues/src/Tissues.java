4d9bff47d5fea4eae84581c76ddaf9015c29ed73
exigow <jakubblach@gmail.com>
2014-10-09 22:48:02 +0200

    Draw order fix

--- tissues/src/Tissues.java
+++ tissues/src/Tissues.java
@@ -22,9 +22,8 @@ public class Tissues implements ApplicationListener {
   private final World world = new World();
   private DebugDraw worldDebugDraw = new DebugDraw(world);
   private ShapeRenderer shapeRenderer;
-  private PolygonSpriteBatch pbatch;
+  private PolygonSpriteBatch polygonSpriteBatch;
   private PolygonRegion quadRegion;
-  private TextureRegion textureRegion;
   private Tissue tissue;
   private final Collection<FingerProcessor> processors = new ArrayList<FingerProcessor>() {{
     for (int i = 0; i < 4; i++)
@@ -36,13 +35,13 @@ public class Tissues implements ApplicationListener {
     Vector size = new Vector(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
     camera = new OrthographicCamera(size.x, size.y);
     shapeRenderer  = new ShapeRenderer();
-    pbatch = new PolygonSpriteBatch();
+    polygonSpriteBatch = new PolygonSpriteBatch();
     TextureRegion textureRegion = new TextureRegion(new Texture(Gdx.files.internal("data/troll.png")));
     quadRegion = new PolygonRegion(textureRegion, new float[8], new short[] {0, 1, 2, 1, 2, 3});
     float[] coords = new float[] {0f, 0f, 1f, 0f, 0, 1f, 1f, 1f};
     for (int i = 0; i < 8; i++)
       quadRegion.getTextureCoords()[i] = coords[i];
-    tissue = new Tissue(new Vector(0f, 0f), new Vector(512, 512), 16, .25f).flush(world);
+    tissue = new Tissue(new Vector(0f, 0f), new Vector(512, 512), 16, .5f).flush(world);
   }
 
   @Override
@@ -53,15 +52,14 @@ public class Tissues implements ApplicationListener {
     world.simulate(Gdx.graphics.getDeltaTime(), 4);
     Gdx.gl.glClearColor(.075f, .075f, .075f, 1f);
     Gdx.gl.glClear(GL11.GL_COLOR_BUFFER_BIT);
-    for (FingerProcessor processor : processors)
-      processor.draw(shapeRenderer);
+    polygonSpriteBatch.setProjectionMatrix(camera.combined);
+    polygonSpriteBatch.begin();
+    tissue.draw(polygonSpriteBatch, quadRegion);
+    polygonSpriteBatch.end();
     shapeRenderer.setProjectionMatrix(camera.combined);
     worldDebugDraw.drawAll(shapeRenderer);
-
-    pbatch.setProjectionMatrix(camera.combined);
-    pbatch.begin();
-    tissue.draw(pbatch, quadRegion);
-    pbatch.end();
+    for (FingerProcessor processor : processors)
+      processor.draw(shapeRenderer);
   }
 
   @Override
