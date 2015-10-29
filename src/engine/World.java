48e2b8c125eb8a05862a3ccdda90b88655c296ce
Jakub Błach <jakubblach@gmail.com>
2015-10-29 19:19:37 +0100

    Usunięcie collision-demo

--- src/engine/World.java
+++ src/engine/World.java
@@ -1,11 +1,8 @@
 package engine;
 
-import engine.joints.CollisionJoint;
 import engine.joints.Joint;
 import org.joml.Vector2f;
-import org.joml.Vector3f;
 
-import java.awt.geom.Line2D;
 import java.util.ArrayList;
 import java.util.Collection;
 
@@ -27,41 +24,6 @@ public class World {
     for (int iteration = 0; iteration < iterations; ++iteration)
       for (Joint joint : joints)
         joint.relax(delta);
-    for (Particle particle : particles) {
-      for (Joint joint : joints) {
-        if (joint instanceof CollisionJoint) {
-          CollisionJoint collision = (CollisionJoint) joint;
-          if (collision.a == particle || collision.b == particle)
-            continue;
-          if (areIntersecting(particle.pos, particle.prev, collision.a.pos, collision.b.pos)) {
-            Vector2f movement = new Vector2f(particle.prev).sub(particle.pos);
-
-            Vector3f movement3 = new Vector3f(movement.x, movement.y, 0);
-            Vector3f normal3 = new Vector3f(collision.normal.x, collision.normal.y, 0);
-            Vector3f reflection3 = new Vector3f(movement3).reflect(normal3).mul(.75f);
-            Vector2f reflection = new Vector2f(reflection3.x, reflection3.y);
-
-
-            particle.reset(particle.prev.x, particle.prev.y);
-            particle.prev.add(reflection);
-
-            reflection.mul(.5f);
-            collision.a.prev.sub(reflection);
-            collision.b.prev.sub(reflection);
-
-          }
-        }
-      }
-    }
-  }
-
-  private static boolean areIntersecting(Vector2f aStart, Vector2f aEnd, Vector2f bStart, Vector2f bEnd) {
-    return Line2D.linesIntersect(
-      aStart.x, aStart.y,
-      aEnd.x, aEnd.y,
-      bStart.x, bStart.y,
-      bEnd.x, bEnd.y
-    );
   }
 
 }
